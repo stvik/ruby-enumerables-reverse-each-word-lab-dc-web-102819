0a58aed1bee6afe7889bca7cbae5f26599928080
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 19:29:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   arr = sentence.split(' ')
-  new_arr = arr.map{|word| word.reverse}
+  new_arr = arr.collect{|word| word.reverse}
   new_arr = new_arr.join(' ')
   return new_arr
 end
